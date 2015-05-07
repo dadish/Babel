7bbb9944b239793a437f6c7a2548b287662f8830
Nurguly Ashyrov <dadish.na@gmail.com>
2015-05-07 11:03:12 +0500

    Refactor the js, create a translateForm object that handles a single language instance. Add extra action buttons for selected translation page. Update the action buttons when the selected translation page is changed. Update the extra options (remove, overwrite) when the translation page is unlinked. Update the notice about the name duplication when the parent of the translation page is changed.

--- ProcessBabelTranslate.js
+++ ProcessBabelTranslate.js
@@ -105,122 +105,145 @@ $(document).ready(function () {
 	settings = config.ProcessBabelTranslate;
 	settings.ajaxUrl = config.urls.admin + 'page/babel-translate/';
 	settings.ajaxNameExistsUrl = config.urls.admin + 'page/add/exists';
+	settings.pageEditUrl = config.urls.admin + 'page/edit/';
 	settings.batchForm = !!$('[data-babel-batch-form="1"]').length;
 	settings.translateForm = !!$('[data-babel-translate-form="1"]').length;
 
 	// Listen to the change in babel_object_$language
 	// inputfields and ask what to do via ajax from 
 	// ProcessBabelTranslate
-	if (settings.translateForm) {
-		(function () {
-			var language, babelObject, babelObjectSelect, babaelName, status, note;
 
-			settings.progress = {};
+	function TranslateForm (options) {
+		this.language = options.language;
+		this.$el = $(options.el);
+		this._progress = null;
+		this.$babelName = this.$('#Inputfield_babel_name_'+ this.language.name);
+		this.$babelParent = this.$('#Inputfield_babel_parent_' + this.language.name)
 
-			function replaceDescription (id, data) {
-				var target, text, key;
-				target = $('#' + id + ' > .InputfieldContent > .description');
-				text = target.text();
-				for (key in data) {
-					text = text.replace('{{'+ key +'}}', '"' + data[key] + '"');
-				}
-				target.text(text);
-			}
+		// add a status wrapper right beside the label
+		this._status = $('<span></span>');
+		this.$('[for="Inputfield_babel_name_'+ language.name +'"]').append(this._status);
+		
+		// add a dupe note
+		this._note = $('<p class="notes">' + this.$babelName.attr('data-note') + '</p>');
+		this._note.insertAfter(this.$babelName);
+		this._note.hide(200);
 
-			function onObjectChange (language) {
-				var id;
-				return function (e, data) {
-					if (!settings.progress[language.name]) {
-						settings.progress[language.name] = $('<li class="ProcessBabelTranslateProgress"><i class="icon fa fa-fw fa-spinner fa-spin"></i></li>');
-						$('#Inputfield_babel_' + language.name + '> .InputfieldContent > .Inputfields').append(settings.progress[language.name]);
-					}
-					settings.progress[language.name].fadeIn();
-					$.get(settings.ajaxUrl + "?action=fieldObject&id="+ settings.pageId +"&object=" + data.id + "&language=" + language.id, function (data) {
-						settings.progress[language.name].fadeOut(0);
-						data = $.parseJSON(data);
-						if (data.follow === 'dependency') {
-							id = data.inputfieldId + '_' + language.name;
-							replaceDescription(id, data.replace);
-							$('#Inputfield_babel_hidden_' + language.name).val(data.hiddenValue).trigger('change');
-						}
-					});
-				};
-			};
+		// listen to page change on babel_object
+		this.$el.on('pageSelected', '#Inputfield_babel_object_'+ language.name, bind(this.onObjectChange, this));
 
-			function onNameChange (language, status, note) {
-				var target, parent, name;
-				return function (ev) {
-					target = $(ev.target);
-					parent = $('[name="babel_parent_' + language.name + '"]');
-					$.get(settings.ajaxNameExistsUrl, {
-						parent_id : parent.val(),
-						name : target.val()
-					}, function (data) {
-						status.html(' ' + data);
-						if ($(data).hasClass('taken')) {
-							target.addClass('ui-state-error-text');
-							note.show(200);
-						} else {
-							target.removeClass('ui-state-error-text');
-							note.hide(200);
-						}
-					});
-				}
-			}
+		// listen to name and parent change and notify about the dupes
+		this.$babelName.on('keyup', bind(debounce(this.onNameChange, 250), this));
+		this.$babelParent.on('pageSelected', bind(this.onNameChange, this));
 
-			function unlinkObject (target, language) {
-				var $input, $listRoot;
-				return function (ev) {
-					$input = target.children('input#Inputfield_babel_object_' + language.name);
-					$listRoot = target.children('.PageListRoot');
+		// bind event on babelActionEdit
+		this.$('.PageListActionsBabel').on('click', 'li.PageListActionEdit a', function (ev) {
+			ev.preventDefault();
+			parent.$('body').trigger('babel:action:edit', $(ev.target).attr('href'));
+		});
 
-					$listRoot.find('*').off();
-					$listRoot.remove();
-					$input.val('');
-					$input.ProcessPageList(config.ProcessBabelTranslate.translateSelectSettings);
-				}
-			}
+		// bind event on babelActionView
+		this.$('.PageListActionsBabel').on('click', 'li.PageListActionView a', function (ev) {
+			ev.preventDefault();
+			parent.$('body').trigger('babel:action:view', $(ev.target).attr('href'));
+		});
 
-			for (var i = 0; i < settings.languages.length; i++) {
-				language = settings.languages[i];
-				if (language.name === settings.currentLanguageName) continue;
-
-				// listen to page change on babel_object
-				babelObject = $('[name="babel_object_'+ language.name +'"]');
-				babelObject.on('pageSelected', onObjectChange(language));
-
-				// listen to name change and notify about the dupes
-				babelName = $('[name="babel_name_'+ language.name +'"]');
-				
-				// add a status wrapper right beside the label
-				status = $('<span></span>');
-				$('[for="Inputfield_babel_name_'+ language.name +'"]').append(status);
-				
-				// add a dupe note
-				note = $('<p class="notes">' + babelName.attr('data-note') + '</p>');
-				note.insertAfter(babelName);
-				note.hide(200)
-				
-				babelName.on('keyup', debounce(onNameChange(language, status, note), 250));
-
-				// bind event on babelActionEdit
-				$('#Inputfield_babel_'+ language.name +' .PageListActionsBabel').on('click', 'li.PageListActionEdit a', function (ev) {
-					ev.preventDefault();
-					parent.$('body').trigger('babel:action:edit', $(ev.target).attr('href'));
-				});
-
-				// bind event on babelActionView
-				$('#Inputfield_babel_'+ language.name + ' .PageListActionsBabel').on('click', 'li.PageListActionView a', function (ev) {
-					ev.preventDefault();
-					parent.$('body').trigger('babel:action:view', $(ev.target).attr('href'));
-				});
-
-				// bind event on babelActionUnlink
-				babelObjectSelect = $('#Inputfield_babel_'+ language.name + ' #wrap_Inputfield_babel_object_' + language.name + ' .InputfieldContent');
-				$('#Inputfield_babel_'+ language.name + ' .PageListActionsBabel').on('click', 'li.PageListActionUnlink a', unlinkObject(babelObjectSelect, language));				
-			}						
-		})();
+		// bind event on babelActionUnlink
+		this.$('.PageListActionsBabel').on('click', 'li.PageListActionUnlink a', bind(this.unlinkObject, this));
 	}
+
+	TranslateForm.prototype.$ = function(selector) {
+		return this.$el.find(selector);
+	};
+
+	TranslateForm.prototype.replaceDescription = function (id, data) {
+		var target, text, key;
+		target = this.$('#' + id + ' > .InputfieldContent > .description');
+		text = target.text();
+		for (key in data) {
+			text = text.replace('{{'+ key +'}}', '"' + data[key] + '"');
+		}
+		target.text(text);		
+	};
+
+	TranslateForm.prototype.onObjectChange = function (ev, data) {
+		if (!this._progress) {
+			this._progress = $('<li class="ProcessBabelTranslateProgress"><i class="icon fa fa-fw fa-spinner fa-spin"></i></li>');
+			this.$('.InputfieldContent .Inputfields').append(this._progress);
+		}
+		this._progress.fadeIn();
+		$.get(settings.ajaxUrl, {
+			action : 'fieldObject',
+			id : settings.pageId,
+			object : data.id,
+			language : this.language.id
+		}, bind(this.onDataRecieve, this));
+		this.updateBabelActions(data);
+	};
+
+	TranslateForm.prototype.onDataRecieve = function(data) {
+		this._progress.fadeOut(0);
+		data = $.parseJSON(data);
+		if (data.follow === 'dependency') {
+			id = data.inputfieldId + '_' + this.language.name;
+			this.replaceDescription(id, data.replace);
+			this.$('#Inputfield_babel_hidden_' + this.language.name).val(data.hiddenValue).trigger('change');
+		}
+	};
+
+	TranslateForm.prototype.onNameChange = function() {
+		function then (data) {
+			this._status.html(' ' + data);
+			if ($(data).hasClass('taken')) {
+				this.$babelName.addClass('ui-state-error-text');
+				this._note.show(200);
+			} else {
+				this.$babelName.removeClass('ui-state-error-text');
+				this._note.hide(200);
+			}
+		}
+
+		$.get(settings.ajaxNameExistsUrl, {
+			parent_id : this.$babelParent.val(),
+			name : this.$babelName.val()
+		}, bind(then, this));
+	};
+
+	TranslateForm.prototype.unlinkObject = function(ev) {
+		var $input, $listRoot;
+		ev.preventDefault();
+		$input = this.$('#Inputfield_babel_object_' + this.language.name);
+		$listRoot = this.$('#wrap_Inputfield_babel_object_'+ this.language.name +' .PageListRoot');
+
+		$listRoot.find('*').off();
+		$listRoot.remove();
+		$input.off();
+		$input.val('');
+		$input.ProcessPageList(config.ProcessBabelTranslate.translateSelectSettings);
+		$input.trigger('pageSelected', {id : 0});
+	};
+
+	if (settings.translateForm) {
+		var TranslateForms, el, language;
+		TranslateForms = {};
+		for (var i = 0; i < settings.languages.length; i++) {
+			language = settings.languages[i];
+			el = '#Inputfield_babel_' + language.name;
+			TranslateForms[language.name] = new TranslateForm({language : language, el : el});
+		}
+	}
+
+	TranslateForm.prototype.updateBabelActions = function(data) {
+		var $a;
+
+		// Change href for actionEdit
+		$a = this.$('.PageListActionsBabel li.PageListActionEdit a');
+		$a.attr('href', (data.id) ? settings.pageEditUrl + '?id=' + data.id : '#');
+
+		// change href for ectionView
+		$a = $a = this.$('.PageListActionsBabel li.PageListActionView a');
+		$a.attr('href', (data.id) ? data.url : '#');
+	};
 
 
 	// If we are on a batch translationPage
